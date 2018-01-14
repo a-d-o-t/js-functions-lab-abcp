a325f7b1aaaf009b1b8f47af98f7cec26038d2c0
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 05:10:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // write your code below!
+
+//ONE
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+
+//TWO
+function happyHolidaysTo(name)
+{
+  return `Happy holidays, ${name}!`
+}
+
+//THREE
+function happyHolidayTo(holiday, name)
+{
+  return `Happy ${holiday}, ${name}!`
+}
+
+//FOUR
+function holidayCountdown(holiday, daysLeft)
+{
+  return `It's ${daysLeft} days until ${holiday}!`
+}
